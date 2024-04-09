1dce4597ae544673e7538e31fa7995b6aa560455
David Lin <d2955388365@gmail.com>
2024-04-09 22:05:32 +0800

    first version

---
+++ README.md
@@ -0,0 +1,8 @@
+# StudentPortal
+ASP.NET Core MVC project
+## Demo
+
+![image](https://res.cloudinary.com/davidlin/image/upload/v1712671014/add_dgiruh.png)
+![image](https://res.cloudinary.com/davidlin/image/upload/v1712671054/list_syyroh.png)
+![image](https://res.cloudinary.com/davidlin/image/upload/v1712671070/delete_gcyxeg.png)
+
---
+++ StidentPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StidentPortal.Models.Entities;
+
+namespace StidentPortal.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
